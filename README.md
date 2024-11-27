2c85e86fda8d7593a2d8aad3e1c8e6371e9e33cd
Mano <mano19029@hotmail.fr>
2024-11-27 13:37:48 +0100

    Update README and man page to include additional conversion specifiers and flags; enhance comments in print_address and print_binary functions for clarity

--- README.md
+++ README.md
@@ -2,6 +2,20 @@
 
 ## Description 
 This project implements a custom `_printf` function that produces output according to a format string. The function mimics the behavior of the standard C library function printf, but with a limited set of conversion specifiers.
+The available conversion specifiers are: c, s, d, i, %, b, u, o, x, X, S, p.
+
+### Conversion Specifiers
+- **c**: The `int` argument is converted to an `unsigned char`, and the resulting character is written.
+- **s**: The `char *` argument is expected to be a pointer to an array of characters. Characters from the array are written up to (but not including) a terminating null byte (`'\0'`).
+- **d, i**: The `int` argument is converted to a signed decimal notation.
+- **%**: A '%' is written. No argument is converted. The complete conversion specification is '%%'.
+- **b**: The `unsigned int` argument is converted to binary notation.
+- **u**: The `unsigned int` argument is converted to unsigned decimal notation.
+- **o**: The `unsigned int` argument is converted to unsigned octal notation.
+- **x**: The `unsigned int` argument is converted to unsigned hexadecimal notation, using the letters "abcdef".
+- **X**: The `unsigned int` argument is converted to unsigned hexadecimal notation, using the letters "ABCDEF".
+- **S**: The `char *` argument is expected to be a pointer to an array of characters. Non-printable characters (ASCII value < 32 or >= 127) are printed this way: `\x`, followed by the ASCII code value in hexadecimal (upper case - always 2 characters).
+- **p**: The `void *` pointer argument is printed in hexadecimal.
 
 ## Compilation
 The code should be compiled using the following command:
--- fonction3.c
+++ fonction3.c
@@ -10,39 +10,39 @@
  */
 int print_address(va_list list, flags_t flags)
 {
-	unsigned long num;
-	int count = 0;
-	int result[32];
-	int i = 0;
+	unsigned long num; /* Déclaration de la variable num */
+	int count = 0; /* Initialisation du compteur */
+	int result[32]; /* Tableau pour stocker les résultats */
+	int i = 0; /* Initialisation de l'index */
 
-	(void)flags;
-	num = va_arg(list, unsigned long);
-	if (num == 0)
+	(void)flags; /* Ignorer les flags */
+	num = va_arg(list, unsigned long); /* Récupérer l'argument */
+	if (num == 0) /* Si le nombre est 0 */
 	{
-		_putchar('0');
-		count++;
+		_putchar('0'); /* Afficher 0 */
+		count++; /* Incrémenter le compteur */
 	}
 	else
 	{
-		_putchar('0');
-		_putchar('x');
-		count += 2;
-		while (num > 0)
+		_putchar('0'); /* Afficher 0 */
+		_putchar('x'); /* Afficher x */
+		count += 2; /* Incrémenter le compteur de 2 */
+		while (num > 0) /* Tant que num est supérieur à 0 */
 		{
-			result[i] = (num % 16);
-			num = num / 16;
-			i++;
+			result[i] = (num % 16); /* Stocker le reste de la division par 16 */
+			num = num / 16; /* Diviser num par 16 */
+			i++; /* Incrémenter l'index */
 		}
-		for (i = i - 1; i >= 0; i--)
+		for (i = i - 1; i >= 0; i--) /* Parcourir le tableau à l'envers */
 		{
-			if (result[i] < 10)
-				_putchar(result[i] + '0');
+			if (result[i] < 10) /* Si le résultat est inférieur à 10 */
+				_putchar(result[i] + '0'); /* Afficher le chiffre */
 			else
-				_putchar(result[i] - 10 + 'a');
-			count++;
+				_putchar(result[i] - 10 + 'a'); /* Afficher la lettre correspondante */
+			count++; /* Incrémenter le compteur */
 		}
 	}
-	return (count);
+	return (count); /* Retourner le compteur */
 }
 /**
  * print_binary - function that prints a binary
@@ -53,30 +53,30 @@ int print_address(va_list list, flags_t flags)
  */
 int print_binary(va_list list, flags_t flags)
 {
-	unsigned int num = va_arg(list, unsigned int);
-	int count = 0;
-	int result[32];
-	int i = 0;
+	unsigned int num = va_arg(list, unsigned int); /* Récupérer l'argument */
+	int count = 0; /* Initialisation du compteur */
+	int result[32]; /* Tableau pour stocker les résultats */
+	int i = 0; /* Initialisation de l'index */
 
-	(void)flags;
-	if (num <= 0)
+	(void)flags; /* Ignorer les flags */
+	if (num <= 0) /* Si le nombre est inférieur ou égal à 0 */
 	{
-		_putchar(num + '0');
-		count++;
+		_putchar(num + '0'); /* Afficher le chiffre */
+		count++; /* Incrémenter le compteur */
 	}
 	else
 	{
-		while (num > 0)
+		while (num > 0) /* Tant que num est supérieur à 0 */
 		{
-			result[i] = (num % 2);
-			num = num / 2;
-			i++;
+			result[i] = (num % 2); /* Stocker le reste de la division par 2 */
+			num = num / 2; /* Diviser num par 2 */
+			i++; /* Incrémenter l'index */
 		}
-		for (i = i - 1; i >= 0; i--)
+		for (i = i - 1; i >= 0; i--) /* Parcourir le tableau à l'envers */
 		{
-			_putchar(result[i] + '0');
-			count++;
+			_putchar(result[i] + '0'); /* Afficher le chiffre */
+			count++; /* Incrémenter le compteur */
 		}
 	}
-	return (count);
+	return (count); /* Retourner le compteur */
 }
